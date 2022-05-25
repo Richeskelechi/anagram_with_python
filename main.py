220fdba33b237d7c51aee89e23db6c384b028efd
Riches <richeskelechi94.rk@gmail.com>
2022-05-25 16:14:32 +0100

    Anagram Modified

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 # find_anagrams("hello", "check") --> False
 # find_anagrams("below", "elbow") --> True
 
-
 def find_anagram(word, anagram):
     # [assignment] Add your code here
     # i am trying to replace all empty spaces inside and around the word
@@ -14,14 +13,18 @@ def find_anagram(word, anagram):
     if(len(word) != len(anagram)):
         return False
     else:
-    # since the length are the same. I will try to sort them to see if the values are the same. 
+        # since the length are the same. I will try to sort them to see if the values are the same.
         if sorted(word) == sorted(anagram):
             return True
         else:
             return False
 
 
+# collecting the first input from the user
 first_string = input("Provide the first string or word: ")
+# collecting the second input from the user
 second_string = input(
     "Provide the second string to compare with the word above: ")
+
+# calling the function to find anagram between the two words or strings
 print(find_anagram(first_string, second_string))
