3c4c4a96eb109992c26f3d1b28d7d87f8018fbac
Riches <richeskelechi94.rk@gmail.com>
2022-05-25 16:07:54 +0100

    Done with the Anagram task

---
+++ main.py
@@ -0,0 +1,27 @@
+# Check if two words are anagrams
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    # i am trying to replace all empty spaces inside and around the word
+    word = word.replace(" ", "").strip()
+    # i am trying to replace all empty spaces inside and around the word to comapre with the other
+    anagram = anagram.replace(" ", "").strip()
+    # checking to see if the length of the two words are the same. If they are not, Then it is not Anagram
+    if(len(word) != len(anagram)):
+        return False
+    else:
+    # since the length are the same. I will try to sort them to see if the values are the same. 
+        if sorted(word) == sorted(anagram):
+            return True
+        else:
+            return False
+
+
+first_string = input("Provide the first string or word: ")
+second_string = input(
+    "Provide the second string to compare with the word above: ")
+print(find_anagram(first_string, second_string))
